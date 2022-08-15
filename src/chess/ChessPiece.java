cee8c9a61c29fb3048a88dd14aca74cb462bd947
Leianny Poiani <leianny.poiani@gmail.com>
2022-08-15 13:45:12 -0300

    Fix isThereOpponentPiece Class + Pawn

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -34,7 +34,7 @@ public abstract class ChessPiece extends Piece{ // só por enquanto
 		return ChessPosition.fromPosition(position);
 	}
 
-	protected boolean isThereOpponentPiece(Position psition) {
+	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
